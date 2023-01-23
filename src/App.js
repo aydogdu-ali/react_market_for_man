c8b863ccf88046f2e7b6bc58055a0f776c203e91
Ali <ali.ozgur@example.com>
2023-01-24 00:27:07 +0300

    add componenets and usestate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import List from './components/List';
+import Alert from './components/Alert';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [text, SetText] = useState("")
+  const [list, setList] = useState([])
+  const [edit, setEdit] = useState(false)
+  const [editId, setEditId] = useState()
+  const [alert, setAlert] = useState({show:false, message:"", type:""})
+
+const handleSubmit= (e)=>{
+  e.preventDefault()
+  console.log("Merhaba Ali")
+
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Alışveriş Listesi</h2>
+
+      <form onSubmit={handleSubmit}>
+        {alert.show && <Alert/>}
+        <input type="text" 
+        placeholder='2 ekmek alırmısın?'
+        value = {text}
+        onChange = {(e)=>SetText(e.target.value)}/>
+        <button  className='ekle-btn' type='submit'>{edit ? "Düzenle": "Ekle"}</button>
+      </form>
+      <div className="container">
+        <h2>Listem</h2>
+        <List />
+        <button className='temizle-btn'>Tümünü Sil</button>
+      </div>
     </div>
   );
 }
