1948b241a626199269f4541689e77dec81ce4d84
Ali <ali.ozgur@example.com>
2023-01-24 18:10:08 +0300

    edit Alert.jsx

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
-const Alert = () => {
+
+const Alert = ({ type, message, removeAlert }) => {
+
+  // verilen uyarının 3 saniye sonra kaybolması için
+  // useEffect kullandım.
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      removeAlert();
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, []);
   return (
-    <div>Alert</div>
-  )
-}
+    <div>
+      <p className={`alert alert-${type}`}>{message}</p>
+    </div>
+  );
+};
 
-export default Alert
+export default Alert;
